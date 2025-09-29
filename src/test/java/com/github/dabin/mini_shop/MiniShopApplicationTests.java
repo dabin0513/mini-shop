fff451bb2e925b2f3695c1dac4c2c1c877812b9f
dabin <ekqls@sweetk.co.kr>
2025-09-29 14:01:04 +0900

    🎉 프로젝트 생성

---
+++ src/test/java/com/github/dabin/mini_shop/MiniShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dabin.mini_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
