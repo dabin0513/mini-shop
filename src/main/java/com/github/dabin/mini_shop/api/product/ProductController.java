eb04345bc93983de9d92f2ca06caf87167f41b48
dabin <dabin0513@naver.com>
2025-10-02 14:06:53 +0900

    :recycle: 응답 객체 수정

--- src/main/java/com/github/dabin/mini_shop/api/product/ProductController.java
+++ src/main/java/com/github/dabin/mini_shop/api/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.github.dabin.mini_shop.api.product;
 
+import com.github.dabin.mini_shop.api.common.response.ApiResponse;
 import com.github.dabin.mini_shop.api.product.dto.request.CreateProductRequestDto;
 import com.github.dabin.mini_shop.api.product.dto.response.CreateProductResponseDto;
 import com.github.dabin.mini_shop.api.product.dto.response.GetProductResponseDto;
@@ -7,6 +8,7 @@ import com.github.dabin.mini_shop.application.product.ProductApplicationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,13 +22,13 @@ public class ProductController {
 
     @GetMapping("/v1/all")
     @Operation(summary = "상품 목록 조회", description = "상품의 전체 리스트를 조회합니다.")
-    public List<GetProductResponseDto> getAll() {
-        return productApplicationService.findAll();
+    public ResponseEntity<ApiResponse<List<GetProductResponseDto>>> getAll() {
+        return ResponseEntity.ok(ApiResponse.success(productApplicationService.findAll()));
     }
 
     @PostMapping("/v1/create")
     @Operation(summary = "상품 단건 등록", description = "상품 정보를 등록합니다.")
-    public CreateProductResponseDto create(@RequestBody CreateProductRequestDto createProductRequestDto) {
-        return productApplicationService.save(createProductRequestDto);
+    public ResponseEntity<ApiResponse<CreateProductResponseDto>> create(@RequestBody CreateProductRequestDto createProductRequestDto) {
+        return ResponseEntity.ok(ApiResponse.success(productApplicationService.save(createProductRequestDto)));
     }
 }
